c20b9035e52418b0dc77af39e9236d828f289bca
raphspies <raphspies@gmail.com>
2022-09-21 19:36:47 -0700

    added basic vigenere cipher: returns encrypt + startingKey

--- vigenere.py
+++ vigenere.py
@@ -9,20 +9,17 @@ def createTable(length, startingKey):
     for a in range(0, fullAlphabets + 1):
       table += LETTERS
     table += LETTERS[0:(subtractLetters + addLetters)]
+    print(table)
     return table
 
 def vigenere(plaintext):
   plaintext = plaintext.lower()
-  print(len(plaintext))
 
   subtractKey = random.randint(0, 25)
   cipherTable = createTable(len(plaintext), subtractKey)
   encoded = ''
-  print(cipherTable)
-  print(len(cipherTable))
   for i in range(0, len(plaintext)):
     currentLetter = plaintext[i]
-    print(i, cipherTable[LETTERS.find(currentLetter) + i])
     if (LETTERS.find(currentLetter) != -1):
       cipherLetter = cipherTable[LETTERS.index(currentLetter) + i]
       encoded += cipherLetter
